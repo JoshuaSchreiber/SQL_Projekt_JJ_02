53934ac77556468316a8d81d6f5b80add0fce150
joshua <joshua.schreiber.bg@bs-gn.de>
2024-06-19 10:55:59 +0200

    ads

--- Scripts/dropDownMenu.js
+++ Scripts/dropDownMenu.js
@@ -1,11 +1,31 @@
-function selectOption(option) {
-    document.getElementById('textField').value = option;
+let titles = ["1. Showing Tables", "2. Select Everything", "Select where ..."];
+let content = [
+    "In our databank we have a certain amount of tables. To work with those tables it is important to know their names. To give out the names of all the tables in our databank, we can use the command: show tables. Try it out. If you give in a wrong command, no problem. The databank simply won't answer.",
+    "Now that we now which tables there are in our databank, we would like to get the content of those tables. To give out the whole content of one table you use the select term together with the * note and the name from the table. Example: SELECT * from AddsD. Give out the content of the Project Table.",
+    "In the last task we learned to give out the whole table, now we wont to give out one line of theat table for that we use the 'where' clause together with the select command. For example one could do 'Select * From Project where ProjektName_pk = 'NextGen Game Engine'. Give out the Project where the name is:HR Management System"
+];
+
+
+
+
+function selectOption(headerText, contentText) {
+    document.getElementById('textFieldHeader').textContent = headerText;
+    document.getElementById('textFieldContent').textContent = contentText;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', (event) => {
     var dropdown = document.getElementsByClassName('dropdown')[0];
     dropdown.addEventListener('click', function() {
         this.getElementsByClassName('dropdown-content')[0].classList.toggle('show');
+    });
+
+    var links = document.querySelectorAll('.dropdown-content a');
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            var index = this.getAttribute('data-index');
+            selectOption(titles[index], content[index]);
+        });
     });
 
     window.onclick = function(event) {
@@ -19,4 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     }
+
+    // Show the first task when the page loads
+    selectOption(titles[0], content[0]);
 });
